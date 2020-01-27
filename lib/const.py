cfb843ec75a8143e9117b8e98b42c271d9f556d7
wuulong <wuulong@gmail.com>
2020-01-27 20:35:23 +0800

    fix: lib/output directory

---
+++ lib/const.py
@@ -0,0 +1,25 @@
+# @file const.py
+# @brief global constant definition
+# README
+# MODULE_ARCH:  
+# CLASS_ARCH: 
+# GLOBAL USAGE: 
+
+#standard
+import os
+
+LSIM_TITLE='Epidemic Prevention Simulator'
+LSIM_VERSION='0.0.1'
+
+AP_MODE_NORMAL=1
+AP_MODE_UNITTEST=2
+
+SR_C0=0
+SR_C1=1
+SR_C2=2
+SR_C3=3
+SR_C4=4
+SR_C5=5
+SR_C6=6
+SR_C7=7
+
